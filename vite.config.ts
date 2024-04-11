ce4a321b3314cbfa0c7c91c4724e1f8a602835f9
Gasu <zgas@mail.ru>
2024-04-11 20:12:40 +0500

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
-import path from 'path'
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.join(__dirname, "docs"),
+  },
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   plugins: [react()],
